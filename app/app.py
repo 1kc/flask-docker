3ffcb840a618fa42f701e5fe34b8bd9bf89906c3
Ken Chen <kenc.360@outlook.com>
2019-05-18 15:23:23 +1000

    mounting vols working

--- app/app.py
+++ app/app.py
@@ -1,9 +1,14 @@
 from flask import Flask
 app = Flask(__name__)
 
+# The main flask app
+
 @app.route("/")
 def hello():
-    return "<h1 style='color:blue'>Hello There!</h1>"
+    return "<h1 style='color:green'>Hello There!</h1>"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    # app.run(host='0.0.0.0')
+
+    # Dev server
+    app.run(host='0.0.0.0', debug=True)
