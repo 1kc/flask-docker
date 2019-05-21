689f0328e570e27ce72bbbf1df7c39fb8717a499
Ken Chen <kenc.360@outlook.com>
2019-05-21 20:58:36 +1000

    modified schema

--- app/build_database.py
+++ app/build_database.py
@@ -1,20 +1,34 @@
 import os
 from config import db
-from models import Person
+from models import Sample
 
 # Data to initialize database with
+'''
 PEOPLE = [
     {"fname": "Doug", "lname": "Farrell"},
     {"fname": "Kent", "lname": "Brockman"},
     {"fname": "Bunny", "lname": "Easter"},
+]
+'''
+
+SAMPLE = [
+    {"l_value": 2.0, "harmful": True, "photo": "photo1.jpg"},
+    {"l_value": 1.5, "harmful": False, "photo": "photo2.jpg"},
+    {"l_value": 10.5, "harmful": True, "photo": "photo3.jpg"},
 ]
 
 # Create the database
 db.create_all()
 
 # iterate over the PEOPLE structure and populate the database
+'''
 for person in PEOPLE:
     p = Person(lname=person.get("lname"), fname=person.get("fname"))
     db.session.add(p)
+'''
+
+for sample in SAMPLE:
+    s = Sample(l_value=sample.get("l_value"), harmful=sample.get("harmful"), photo=sample.get("photo"))
+    db.session.add(s)
 
 db.session.commit()
--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from config import db, ma
 
 
+"""
 class Person(db.Model):
     __tablename__ = "person"
     person_id = db.Column(db.Integer, primary_key=True)
@@ -15,4 +16,22 @@ class Person(db.Model):
 class PersonSchema(ma.ModelSchema):
     class Meta:
         model = Person
+        sqla_session = db.session
+
+"""
+
+class Sample(db.Model):
+    __tablename__ = "sample"
+    grain_id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(
+        db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow
+    )
+    l_value = db.Column(db.Numeric())
+    harmful = db.Column(db.Boolean())
+    photo = db.Column(db.String(32))
+
+
+class SampleSchema(ma.ModelSchema):
+    class Meta:
+        model = Sample
         sqla_session = db.session
