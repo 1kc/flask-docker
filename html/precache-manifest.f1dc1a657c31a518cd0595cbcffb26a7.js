927bfd4030262d9bd08586c2b70d850cfe4cbac8
Ken Chen <kenc.360@outlook.com>
2019-05-21 23:25:08 +1000

    test full stack deployment

---
+++ html/precache-manifest.f1dc1a657c31a518cd0595cbcffb26a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a3c8eb9b872d0f5a4fd8951cf844af9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45dfe8334f0aeabb6911",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "696c7e6f0997a57a4801",
+    "url": "/static/js/2.4f988591.chunk.js"
+  },
+  {
+    "revision": "45dfe8334f0aeabb6911",
+    "url": "/static/js/main.91e03b72.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
