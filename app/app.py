b7ebffecb985e160111efe0de9cd27cfcecb8ef5
Ken Chen <kenc.360@outlook.com>
2019-05-22 22:03:32 +1000

    removed redundant pkgs

--- app/app.py
+++ app/app.py
@@ -4,13 +4,9 @@ Main module of the server file
 
 # local modules
 import config
-from flask_cors import CORS
 
 # Get the application instance
 connex_app = config.connex_app
-
-# add CORS support
-CORS(connex_app.app)
 
 # Read the swagger.yml file to configure the endpoints
 connex_app.add_api("swagger.yml")
