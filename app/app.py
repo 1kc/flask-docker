c1a3e5615ab1d5bdc0aea9c0ffa9d96c3b55c004
Ken Chen <kenc.360@outlook.com>
2019-05-22 20:02:17 +1000

    frontend

--- app/app.py
+++ app/app.py
@@ -9,10 +9,12 @@ from flask_cors import CORS
 # Get the application instance
 connex_app = config.connex_app
 
+# add CORS support
+CORS(connex_app.app)
+
 # Read the swagger.yml file to configure the endpoints
 connex_app.add_api("swagger.yml")
 
 
 if __name__ == "__main__":
-    CORS(connex_app.app)
     connex_app.run(debug=True)
---
+++ frontend/src/App.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import SearchAppBar from './SearchAppBar';
+import EnhancedTable from './EnhancedTable';
+import axios from 'axios'
+import './App.css';
+
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#FFB300'
+    },
+  },
+});
+
+class App extends React.Component {
+
+  state = {
+    sample: []
+  };
+
+  componentDidMount() {
+    axios
+      .get("http://127.0.0.1:80/api/sample")
+      .then(response => {
+        this.setState((state, props) => ({sample: response.data}));
+        /*
+        // create an array of contacts only with relevant data
+        const newContacts = response.data.map(c => {
+          return {
+            id: c.id,
+            name: c.name
+          };
+        });
+
+        // create a new "State" object without mutating 
+        // the original State object. 
+        const newState = Object.assign({}, this.state, {
+          contacts: newContacts
+        });
+
+        // store the new state object in the component's state
+        this.setState(newState);
+        */
+
+      })
+      .catch(error => console.log(error));
+  }
+
+  render() {
+    return (
+      <MuiThemeProvider theme={theme}>
+      <div className="App">
+        <SearchAppBar />
+        <Grid container spacing={24} justify="center">
+  
+          <Grid item xs={8}>
+            <EnhancedTable sample={this.state.sample}/>
+          </Grid>
+        </Grid>
+      </div>
+      </MuiThemeProvider>
+    );
+  }
+
+}
+
+export default App;
