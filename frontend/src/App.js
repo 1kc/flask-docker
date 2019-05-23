b35aa5146a3ca06d390c7abf5d6d726246543f8f
Ken Chen <kenc.360@outlook.com>
2019-05-23 23:47:05 +1000

    adding on frontend working

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
 import SearchAppBar from './SearchAppBar';
 import EnhancedTable from './EnhancedTable';
+import Inputs from './Inputs';
 import axios from 'axios'
 import './App.css';
 
@@ -26,25 +28,6 @@ class App extends React.Component {
       .get(`/api/sample`)
       .then(response => {
         this.setState((state, props) => ({sample: response.data}));
-        /*
-        // create an array of contacts only with relevant data
-        const newContacts = response.data.map(c => {
-          return {
-            id: c.id,
-            name: c.name
-          };
-        });
-
-        // create a new "State" object without mutating 
-        // the original State object. 
-        const newState = Object.assign({}, this.state, {
-          contacts: newContacts
-        });
-
-        // store the new state object in the component's state
-        this.setState(newState);
-        */
-
       })
       .catch(error => console.log(error));
   }
@@ -54,9 +37,18 @@ class App extends React.Component {
       <MuiThemeProvider theme={theme}>
       <div className="App">
         <SearchAppBar />
-        <Grid container spacing={24} justify="center">
-  
+        <Grid container spacing={8} justify="center">
+
+
           <Grid item xs={8}>
+            <br />
+            <Paper>
+              <Inputs />
+            </Paper>
+          </Grid>
+
+          <Grid item xs={8}>
+
             <EnhancedTable sample={this.state.sample}/>
           </Grid>
         </Grid>
---
+++ frontend/src/Inputs.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Input from '@material-ui/core/Input';
+import IconButton from '@material-ui/core/IconButton';
+import AddCircle from '@material-ui/icons/AddCircle';
+import axios from 'axios'
+
+const styles = theme => ({
+  input: {
+    margin: theme.spacing.unit,
+  },
+});
+
+// function Inputs(props) {
+
+class Inputs extends React.Component {
+
+  state = {
+    l_value: '0',
+    harmful: 'No',
+    photo: 'photo.jpg',
+  };
+
+  handleChange = name => event => {
+    this.setState({ [name]: event.target.value });
+  };
+
+  handleClick = event => {
+    console.log(this.state.l_value + ' ' + this.state.harmful + ' ' + this.state.photo);
+    let data = {
+      harmful: (this.state.harmful === 'True' ? true : false),
+      l_value: parseFloat(this.state.l_value),
+      photo: this.state.photo,
+
+    }
+    axios
+      .post('/api/sample', data)
+      .then(function (response) {
+          //handle success
+          console.log(response);
+      })
+      .catch(function (response) {
+          //handle error
+          console.log(response);
+    });
+  };
+
+  render() {
+
+    const { classes } = this.props;
+
+    return (
+      <div>
+        <Input
+          placeholder="L Value"
+          className={classes.input}
+          inputProps={{
+            'aria-label': 'Description',
+          }}
+          onChange={this.handleChange('l_value')}
+        />
+
+
+        <Input
+          placeholder="Detected Harmful"
+          className={classes.input}
+          inputProps={{
+            'aria-label': 'Description',
+          }}
+          onChange={this.handleChange('harmful')}
+        />
+
+        <Input
+          placeholder="Photo URL"
+          className={classes.input}
+          inputProps={{
+            'aria-label': 'Description',
+          }}
+          onChange={this.handleChange('photo')}
+        />
+
+        <IconButton color="primary" 
+          onClick={this.handleClick}
+        >
+          <AddCircle />
+        </IconButton>
+
+      </div>
+    );
+    }
+}
+
+export default withStyles(styles)(Inputs);
